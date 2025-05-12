01bdd8d337bccc2a137f74a64201b38ac5abca8a
Ravella Abhinav <82701555+ravellaabhinav@users.noreply.github.com>
2025-05-12 01:59:28 -0400

    Update work.java

--- work.java
+++ work.java
@@ -105,3 +105,49 @@ if (remitStatus != null)
 					and_operator = and_operator_temp;
 				}
 
+
+
+=== FRUIT 🍉🍎🍊🍐🍋‍🟩 ===
+
+if (remitStatus != null) {
+    and_operator_temp = true;
+    switch (remitStatus) {
+
+        case "Remittance": {
+            String placeholders = getInClausePlaceholders(Constants.ERA_STATUS_PAID, setParameters, Types.VARCHAR);
+            String condition = "c1.last_era_status IN (" + placeholders + ")";
+            RevenueCycleMgmtUtil.create_sql_fragment_no_params(condition, whereClause, and_operator);
+            break;
+        }
+
+        case "NoRemittance": {
+            String placeholders = getInClausePlaceholders(Constants.ERA_STATUS_PAID, setParameters, Types.VARCHAR);
+            String condition = "(c1.last_era_status NOT IN (" + placeholders + ") OR c1.last_era_status IS NULL)";
+            RevenueCycleMgmtUtil.create_sql_fragment_no_params(condition, whereClause, and_operator);
+            break;
+        }
+
+        case "ZeroPaid": {
+            RevenueCycleMgmtUtil.getWhereClauseFragment(
+                whereClause, setParameters, !and_operator,
+                Types.VARCHAR, "c1.last_era_status = ?", Constants.ERA_STATUS_ZERO_PAID);
+            break;
+        }
+
+        case "NonZeroPaid": {
+            RevenueCycleMgmtUtil.getWhereClauseFragment(
+                whereClause, setParameters, !and_operator,
+                Types.VARCHAR, "c1.last_era_status = ?", Constants.ERA_STATUS_NON_ZERO_PAID);
+            break;
+        }
+
+        default: {
+            and_operator_temp = and_operator;
+            break;
+        }
+    }
+    and_operator = and_operator_temp;
+}
+	
+
+	
